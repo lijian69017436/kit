dfb6cc9db6f84a8a80f9cf3515da23507f8d296d
Administrator <Administrator@192.168.0.100>
2015-10-14 21:37:57 +0800

    1

--- 1--all/src/main/java/proxys/ProxyHandler.java
+++ 1--all/src/main/java/proxys/ProxyHandler.java
@@ -5,8 +5,9 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 
-/*
- * 生成与代理类相关联的InvocationHandler对象 
+/*  动态代理 
+ * 
+ *  生成与代理类相关联的InvocationHandler对象 
  */
 public class ProxyHandler implements InvocationHandler {
 	
@@ -14,23 +15,23 @@ public class ProxyHandler implements InvocationHandler {
 	private Object dele;
 	
 	public ProxyHandler(Object obj){
-		this.dele = obj;
+	  this.dele=obj;
 	}
 	/**
 	 * proxy   目标对象的代理类实例
 	 * method	对应在代理实例上调用接口方法的 method实例
 	 * args		传入到代理实例上方法参数值的对象数组 
 	 * return 	方法的返回值  没有返回值 时是null
+	 * 
+	 * @具体的代理层  的 处理程序 
 	 */
-	
 	public Object invoke(Object proxy, Method method, Object[] args)
 			throws Throwable {
 		// TODO Auto-generated method stub
-	
 		
-		//在这里完全可以把下面这句注释掉，而做一些其它的事情  
+		//在这里完全可以  做一些其它的事情  
 		
-		//身份验证
+		//身份验证   ...等等
 		
 		quanxian();//权限满足 继续 下一步
 		//转发
@@ -38,28 +39,10 @@ public class ProxyHandler implements InvocationHandler {
 		
 		return result;
 	}
-	public static void quanxian(){
+	
+	public  void quanxian(){
 		System.out.println("权限 ——》》");
 	}
 	
-	
-	public static Hello proxy(){//代理 方法
-
-		//实例化 真实的 实现类
-		Hello impl = new HelloImpl();
-		
-		//生成相关联的类
-		ProxyHandler handler = new ProxyHandler(impl);
-		/**
-		 * loader    定义代理类的 加载器
-		 * interfaces  代理类 要实现 的接口列表
-		 * h   指派方法调用的 调用处理程序
-		 */
-		//生成代理类，并将handler关联在一起。
-		Hello hello = (Hello)Proxy.newProxyInstance(impl.getClass().getClassLoader(),
-				                                    impl.getClass().getInterfaces(), 
-				                                    handler);
-		
-		return hello;
-	}
 }
+
--- 1--all/src/main/java/proxys/test.java
+++ 1--all/src/main/java/proxys/test.java
@@ -1,14 +1,25 @@
 package proxys;
 
+import java.lang.reflect.Proxy;
 
 public class test {
 
 	
 	public static void main(String[] args) {
-		
-		Hello hello=ProxyHandler.proxy();
-		//这里无论访问哪个方法，都是会把请求转发到handler.invoke  
-		
+	  Hello he = new HelloImpl();
+    /**
+     * 1,把对象加载到代理方法中去     
+     * 2, 中间代理对象处理下     
+     * 3, 然后返回代理对象 
+     * 4, 然后强转成接口   
+     * 5,代理对象  调用方法   
+     */
+    Hello hello = (Hello)Proxy.newProxyInstance(
+        he.getClass().getClassLoader(),  //代理类加载器
+        he.getClass().getInterfaces(),   //代理类实现的接口
+        new ProxyHandler(he));           //代理类的 处理程序    
+    //我代理的 一个产品   可以在这个产品上 加点 价格等等    自定义的内容
+    //反正 买家接触不到  具体 的厂家  你也不知道
 		hello.print("All the test");
 		hello.sayHello("Denny");
 	}
