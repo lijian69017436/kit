109437b8cce6c7005998ef5f415cb6aaaf53803c
lijian <1028332674@qq.com>
2015-10-14 09:50:54 +0800

    1

---
+++ 1--all/src/main/java/design/design/factory/counter/Factory.java
@@ -0,0 +1,93 @@
+package design.design.factory.counter;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class Factory {
+  @Test
+  public void test(){ //客户端 
+    Operation oper;
+    OperationFactory fac=new OperationFactory();
+
+    Scanner sc = new Scanner(System.in);  
+    System.out.print("请输入数字A:");
+    String stra=sc.next();
+    System.out.print("请输入运算符号(+-*/):");
+    String strc=sc.next();
+    System.out.print("请输入数字b:");
+    String strb=sc.next();
+    
+    oper=fac.createOperation(strc);
+    oper.doubleA=Integer.valueOf(stra);
+    oper.doubleB=Integer.valueOf(strb);
+    System.out.println(oper.getResult());
+  }
+  
+}
+
+class Operation{//操作类  (业务对象)  
+  double doubleA=0d;
+  double doubleB=0d;
+  double result=0d;
+  public double getResult(){
+    double result=0d;
+    return result;
+  }
+}
+
+
+ 
+class OperationFactory{   //工厂创建对象 
+   Operation oper;
+  public  Operation createOperation(String operator){
+    switch (operator) {
+      case "+" :
+        oper=new OperationAdd();
+        break;
+      case "-" :
+        oper=new OperationCut();
+        break;
+      case "*" :
+        oper=new OperationRide();
+        break;
+      case "/" :
+        oper=new OperationDivide();
+        break;
+      default :
+        oper=new Operation();
+        break;
+    }  
+    return oper;
+  }
+}
+
+
+class OperationAdd extends Operation  { //业务对象的具体实现
+  @Override
+  public double getResult() {
+    result=doubleA +doubleB;
+    return result;
+  }
+}
+class OperationCut extends Operation  {
+  @Override
+  public double getResult() {
+    result=doubleA -doubleB;
+    return result;
+  }
+}
+class OperationRide extends Operation  {
+  @Override
+  public double getResult() {
+    result=doubleA *doubleB;
+    return result;
+  }
+}
+class OperationDivide extends Operation  {
+  @Override
+  public double getResult() {
+    result=doubleA /doubleB;
+    return result;
+  }
+}
---
+++ 1--all/src/main/java/design/design/factory/counter/Progarm.java
@@ -0,0 +1,100 @@
+package design.design.factory.counter;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+
+public class Progarm {
+  @Test
+  public void progarm() { //最简单的 计算器  面向过程的
+    Scanner sc = new Scanner(System.in);  
+    System.out.print("请输入数字A:");
+    String a=sc.next();
+    System.out.print("请输入运算符号(+-*/):");
+    String c=sc.next();
+    System.out.print("请输入数字b:");
+    String b=sc.next();
+    String d="";
+    if(c.equals("+")){
+      d=(Integer.valueOf(a)+Integer.valueOf(b))+"";
+    }
+    if(c.equals("-")){
+      d=(Integer.valueOf(a)-Integer.valueOf(b))+"";
+    }
+    if(c.equals("*")){
+      d=(Integer.valueOf(a)*Integer.valueOf(b))+"";
+    }
+    if(c.equals("/")){
+      d=(Integer.valueOf(a)/Integer.valueOf(b))+"";
+    }
+    System.out.println(d);
+  }
+  @Test
+  public void progarm1() { //最简单的 计算器  优化命名等
+    Scanner sc = new Scanner(System.in);  
+    System.out.print("请输入数字A:");
+    String stra=sc.next();
+    System.out.print("请输入运算符号(+-*/):");
+    String strc=sc.next();
+    System.out.print("请输入数字b:");
+    String strb=sc.next();
+    String result="";
+    switch (strc) {
+      case "+" :
+        result=(Integer.valueOf(stra)+Integer.valueOf(strb))+"";
+        break;
+      case "-" :
+        result=(Integer.valueOf(stra)-Integer.valueOf(strb))+"";
+        break;
+      case "*" :
+        result=(Integer.valueOf(stra)*Integer.valueOf(strb))+"";
+        break;
+      case "/" :
+        if(!strb.equals("0"))
+          result=(Integer.valueOf(stra)/Integer.valueOf(strb))+"";
+        break;
+      default :
+        result="运算符输入错误";
+        break;
+    }
+    System.out.println(result);
+  }
+  
+  @Test
+  public void progarm2() { //最简单的 计算器  简单的 面向对象的  业务 和页面分离
+    Scanner sc = new Scanner(System.in);  
+    System.out.print("请输入数字A:");
+    String stra=sc.next();
+    System.out.print("请输入运算符号(+-*/):");
+    String strc=sc.next();
+    System.out.print("请输入数字b:");
+    String strb=sc.next();
+    double result=count.getResult(Double.valueOf(stra), Double.valueOf(strb), strc);
+    System.out.println(result+"");
+  }
+  
+}
+class count{
+  public static double getResult(double inputa, double inputb ,String inputCount){
+    double result=0;
+    switch (inputCount) {
+      case "+" :
+        result=(inputa)+inputb;
+        break;
+      case "-" :
+        result=(inputa)-inputb;
+        break;
+      case "*" :
+        result=(inputa)*inputb;
+        break;
+      case "/" :
+        if(inputb!=0)
+          result=(inputa)/inputb;
+        break;
+    }
+    return result;
+  }
+  
+  
+}
