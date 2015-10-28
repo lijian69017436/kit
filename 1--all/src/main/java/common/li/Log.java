f4f6ab55576786a2fff200364b937e256a6876d3
lijian <1028332674@qq.com>
2015-10-28 15:51:34 +0800

    log 配置详解

--- 1--all/src/main/java/common/li/Log.java
+++ 1--all/src/main/java/common/li/Log.java
@@ -21,5 +21,17 @@ public class Log {
   public static void error(String line){
 	  base.error(line);
   }
+  
 }
- 
+//Logger.getLogger("base");   这是 取 出 loger 名称
+//Logger.getLogger(Log.class);  这是 更灵活的  一种方式  这种 可配置性 超高  
+//这样写  是 按照 包 打印       lijian包
+//log4j.logger.lijian=debug, Console,baseLog
+//这样写   是 独立出  rootlog
+//log4j.additivity.lijian=false
+//log4j.appender.baseLog=org.apache.log4j.DailyRollingFileAppender
+//log4j.appender.DatePattern='.'yyyy-MM-dd
+//log4j.appender.baseLog.File=${catalina.base}/logs/lijian.log
+//log4j.appender.baseLog.layout=org.apache.log4j.PatternLayout
+//log4j.appender.baseLog.layout.ConversionPattern=%d %-5p  %-50m  ## %l  [%t] %n
+
