c8d33adb35eddd64865c58a1cea9e0fd5d83c3af
lijian <1028332674@qq.com>
2015-12-22 13:54:02 +0800

    3

--- 1--all/src/test/java/test1.java
+++ 1--all/src/test/java/test1.java
@@ -6,6 +6,9 @@ public class test1 {
   @Test
   public void test2(){
     Log.debug("-----");
+    String i="123123"
+;
+    System.out.println(i.length());
     Log.info(String.format("你好:%s%s",1<<4,"--"));
   }
 }
