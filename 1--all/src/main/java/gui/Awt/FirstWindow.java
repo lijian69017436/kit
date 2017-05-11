8a05e3b027d7bec65a5d1b4fa716f401445426af
lijian <15001062251@163.com>
2017-05-11 17:16:33 +0800

    窗口提示

--- 1--all/src/main/java/gui/Awt/FirstWindow.java
+++ 1--all/src/main/java/gui/Awt/FirstWindow.java
@@ -11,6 +11,11 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class FirstWindow {
 
@@ -31,29 +36,41 @@ public class FirstWindow {
 		bt.addMouseListener(new MouseAdapter() {//鼠标事件监听
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("单击++");
-				System.out.println(tf.getText());
-				
+				 JFrame frame1=new JFrame("第二个窗体");
+					frame1.setBounds(100,100,1800,900);
+					frame1.setLayout(new FlowLayout());//创建一个布局  设置默认布局
+					frame1.setVisible(true);//让窗体可见
+					frame1.setAlwaysOnTop(true);
+					
 			}
 		});
 		
-		bt.addKeyListener(new KeyAdapter() {//键盘事件监听  
-			public void keyPressed(KeyEvent e){//如果 键盘输入 enter 退出
+		
+		tf=new TextField();//添加文本框
+		tf.setColumns(40);
+		tf.addKeyListener(new KeyAdapter() {//键盘事件监听  
+			public void keyPressed(KeyEvent e){//如果 键盘输入 enter 打印文本框内容
 				if(e.getKeyCode()==e.VK_ENTER){
-					System.exit(0);
+//					System.exit(0);
+					System.out.println(tf.getText());
 				}
 			}
 		});
 		
-		tf=new TextField();//添加文本框
-		tf.setColumns(40);
 		
 		frame.setLocation(300,399);//设置窗体在显示器的位置
-		frame.setSize(300,300);//大小 
+		frame.setSize(400,200);//大小 
 		
 		frame.setLayout(new FlowLayout());//创建一个布局  设置默认布局
 		frame.add(bt);//添加按钮
 		frame.add(tf);
+		frame.add(new JLabel("显示文字"));
 		
+		frame.addWindowListener(new WindowAdapter(){ // 为了关闭窗口
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		frame.setVisible(true);//让窗体可见
 	}
 }
